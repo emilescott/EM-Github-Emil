648e537fbfd2598d849d7bdf29d2a814f8a398e5
codescott <EMILE@emilionahs@gmai.com>
2024-04-11 17:32:06 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# emilapp
+
+A new Flutter project.
+
---
+++ android/app/src/main/kotlin/com/example/emilapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.emilapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
